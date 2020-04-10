774967d80f0d2cfee282243cc8849f7bf4ec85ba
Roman <cypherpanda@pm.me>
2020-04-10 19:10:26 +0200

    pages update

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    password = db.Column(db.Text)
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+    def __repr__(self):
+        return(name, password)
 
 
 @app.route('/')
@@ -12,9 +38,15 @@ def index():
 def login():
     return render_template('login.html')
 
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+@app.route('/notes')
+def notes():
+    return render_template('notes.html')
 
 
 if __name__ == '__main__':
